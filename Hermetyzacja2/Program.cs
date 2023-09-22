c127a98b4f45d973334251c2844c7758e62fd353
Tomasz Fryckowski <tomaszfryckowski@gmail.com>
2023-09-22 11:18:35 +0200

    Dodaj pliki projektów.

---
+++ Hermetyzacja2/Program.cs
@@ -0,0 +1,21 @@
+﻿using Hermetyzacja2;
+
+class DaneUżytkownika
+{
+    static void Main(string[] args)
+    {
+        Użytkownik Gosciu = new Użytkownik();
+
+        Console.WriteLine("Podaj swoje Imie");
+
+        Gosciu.imie = Console.ReadLine();
+
+        Console.WriteLine("Podaj swój wiek");
+        Gosciu.wiekProp =  Convert.ToInt32(Console.ReadLine());
+
+
+
+
+    Console.WriteLine($"Jestem {Gosciu.imie} i mam {Gosciu.wiekProp} lat");
+    }
+}
---
+++ Hermetyzacja2/Użytkownik.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Hermetyzacja2
+{
+    public class Użytkownik
+    {
+        public string imie;
+       private int wiek;
+        
+     
+        
+
+        public int wiekProp
+        {
+            get
+            {
+                
+                return wiek;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Nieprawidłowa wartośc ");
+                }
+                else if (value >= 130)
+                {
+                    Console.WriteLine("Nikt nie żyje tyle lat!");
+                }
+                else if (value >= 0 && value <= 129) 
+                wiek = value;
+            }
+        }
+
+    }
+}
